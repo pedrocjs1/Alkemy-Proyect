0b762d100e9f55855bc2f023d7e06007a894318d
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-03-08 14:48:39 -0500

    Update Personaje.java

--- src/main/java/com/challenge/backend/models/Personaje.java
+++ src/main/java/com/challenge/backend/models/Personaje.java
@@ -5,9 +5,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 public class Personaje {
